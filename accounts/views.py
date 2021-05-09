f01baf1b4d1307839c6ec88e4785d00b622418e9
omRubiks <om.rubiks@gmail.com>
2021-05-09 19:41:58 +0530

    done

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,7 @@ from .forms import CreateUserLogForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 # Create your views here.
-def home(request):
-    if request.user.is_anonymous:
-        return redirect("login")
-    
+def home(request):  
     return render(request,'home.html')
     pass
 
@@ -27,13 +24,7 @@ def login_user(request):
             print("User Not Found")
             return redirect('login')
             pass
-            # messages.error(request, "Username Or Password is incorrect!!",
-            #                extra_tags='alert alert-warning alert-dismissible fade show')
 
-    return render(request, 'login.html')
-
-
-    user=UsersAccounts.objects.all()
     return render(request,'login.html')
     pass
 
